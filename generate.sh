422f1ec5b9174cd484264ae45a62d70ec2d69ea3
Asheesh Laroia <asheesh@asheesh.org>
2020-02-14 07:32:57 -0800

    Fix diff parameters

--- generate.sh
+++ generate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Used for developing this repo.
+set -eou pipefail
 
+mkdir -p tmp
 git archive -0 --format zip HEAD > tmp/$USER-cookie-cutter-android.zip
 cd tmp
 cookiecutter --replay ./$USER-cookie-cutter-android.zip
-diff -uRN $USER-cookie-cutter-android gold-standard
+diff -urN $USER-cookie-cutter-android gold-standard
