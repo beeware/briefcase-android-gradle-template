0b72e0160b85f40d3b152451c9beb71c20da8b25
Russell Keith-Magee <russell@keith-magee.com>
2020-03-27 10:08:32 +0800

    Give the generated code a little more breathing room.

--- {{ cookiecutter.formal_name }}/app/src/main/java/org/beeware/android/MainActivity.java
+++ {{ cookiecutter.formal_name }}/app/src/main/java/org/beeware/android/MainActivity.java
@@ -59,20 +59,25 @@ public class MainActivity extends AppCompatActivity {
     private Map<String, File> getPythonDirs() throws IOException {
         Map dirs = new HashMap<String, File>();
         File base = new File(getApplicationContext().getFilesDir().getAbsolutePath() + "/python/");
+
         // `stdlib` is used as PYTHONHOME.
         File stdlib = new File(base.getAbsolutePath() + "/stdlib/");
         ensureDir(stdlib);
         dirs.put("stdlib", stdlib);
+
         // `rubicon_java` is used to the required Rubicon module.
         File rubicon_java = new File(base.getAbsolutePath() + "/rubicon-java/");
         ensureDir(rubicon_java);
         dirs.put("rubicon_java", rubicon_java);
+
         // Put `user_code` into dirs so we can unpack the assets into it.
         dirs.put("user_code", new File(base.getAbsolutePath() + "/user_code/"));
+
         // `app` and `app_packages` store user code and user code dependencies,
         // respectively. These paths exist within the `python/` assets tree.
         File app = new File(base.getAbsolutePath() + "/user_code/app/");
         dirs.put("app", app);
+
         File app_packages = new File(base.getAbsolutePath() + "/user_code/app_packages/");
         dirs.put("app_packages", app_packages);
         return dirs;
@@ -81,23 +86,27 @@ public class MainActivity extends AppCompatActivity {
     private void unpackPython(Map<String, File> dirs) throws IOException {
         String myAbi = Build.SUPPORTED_ABIS[0];
         File pythonHome = dirs.get("stdlib");
+
         Log.d("unpackPython", "Unpacking Python with ABI " + myAbi + " to " + pythonHome.getAbsolutePath());
         unzipTo(new ZipInputStream(this.getAssets().open("pythonhome." + myAbi + ".zip")), pythonHome);
         makeExecutable(new File(pythonHome.getAbsolutePath() + "/bin/python3"));
         makeExecutable(new File(pythonHome.getAbsolutePath() + "/bin/python3.7"));
         File rubicon_java = dirs.get("rubicon_java");
+
         Log.d("unpackPython", "Unpacking rubicon-java to " + rubicon_java.getAbsolutePath());
         unzipTo(new ZipInputStream(this.getAssets().open("rubicon.zip")), rubicon_java);
         File userCodeDir = dirs.get("user_code");
+
         Log.d("unpackPython", "Unpacking Python assets to base dir " + userCodeDir.getAbsolutePath());
         unpackAssetPrefix(getAssets(), "python", userCodeDir);
     }
 
     private void setPythonEnvVars(String pythonHome) throws IOException, ErrnoException {
-        Os.setenv("RUBICON_LIBRARY", this.getApplicationInfo().nativeLibraryDir + "/librubicon.so", true);
         Log.v("python home", pythonHome);
         Context applicationContext = this.getApplicationContext();
         File cacheDir = applicationContext.getCacheDir();
+
+        Os.setenv("RUBICON_LIBRARY", this.getApplicationInfo().nativeLibraryDir + "/librubicon.so", true);
         Os.setenv("TMPDIR", cacheDir.getAbsolutePath(), true);
         Os.setenv("LD_LIBRARY_PATH", this.getApplicationInfo().nativeLibraryDir, true);
         Os.setenv("PYTHONHOME", pythonHome, true);
@@ -108,12 +117,14 @@ public class MainActivity extends AppCompatActivity {
         this.unpackPython(dirs);
         String pythonHome = dirs.get("stdlib").getAbsolutePath();
         this.setPythonEnvVars(pythonHome);
+
         // `app` is the last item in the sysPath list.
         String sysPath = (pythonHome + "/lib/python3.7/") + ":" + dirs.get("rubicon_java").getAbsolutePath() + ":"
                 + dirs.get("app_packages").getAbsolutePath() + ":" + dirs.get("app").getAbsolutePath();
         if (Python.init(pythonHome, sysPath, null) != 0) {
             throw new Exception("Unable to start Python interpreter.");
         }
+
         // Store the code in a file because Python.run() takes a filename.
         // We don't run app/__main__.py directly to avoid problems with package-relative
         // imports.
